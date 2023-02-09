27af8b9a00de099be17070427073840a0b83cf0c
maksimk5 <109814868+maksimkfive@users.noreply.github.com>
2023-02-09 14:02:38 +0400

    Update fuck.py

--- fuck.py
+++ fuck.py
@@ -1,7 +1,6 @@
-spaceindex = 0
 with open('proxy.txt', 'r') as file:
     filteredproxies = open('filteredproxy.txt', 'w')
     for i in file.readlines():
-        spaceindex = i.find(' ')
-        i = i[0:spaceindex]
+        i = i[0:i.find(' ')]
         filteredproxies.write(str(i) + '\n')
+        
